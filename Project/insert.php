1ca2ec0834ea27afc4286fef514092cdd72972c3
shreyasbabu <shreyasbabu@users.noreply.github.com>
2020-11-07 13:34:34 -0500

    php file with redirect to "success" and "user exists" messages

--- Project/insert.php
+++ Project/insert.php
@@ -3,6 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "hstworld";
+$flag = 0;
 
 if (isset($_POST)) {
 $firstname = $_POST['FirstName'];
@@ -25,9 +26,9 @@ if(!$conn){
  	$sql = "INSERT INTO `users` (`first_name`, `last_name`, `username`, `email`, `password`, `phone`) 
 	VALUES ('$firstname', '$lastname', '$uusername', '$email','$upassword','$phone')";
 /*$sql = "INSERT INTO `signin`(`FirstName`, `LastName`, `UserName`, `Email`, `PhoneNumber`, `Password`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6])";*/
-if(mysqli_query($conn, $sql))
+	if(mysqli_query($conn, $sql))
 	{
-    echo "Records added successfully.";
+    $flag = 0;
 	} 
 	else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
@@ -35,7 +36,7 @@ if(mysqli_query($conn, $sql))
  }
  else
  {
- 	echo "User Already Exists Please Login";
+     $flag = 1;
  }
 }
 	
@@ -59,3 +60,51 @@ function UserExists($conn,$user)
 // Close connection
 mysqli_close($conn);
 ?>
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>HSTWorld</title>
+  </head>
+  <body>
+
+  	<?php
+  	if($flag == 0){
+  		?>
+  		<div class="alert alert-success alert-dismissible fade show" role="alert">
+  			<h1>Registered Sucessfully</h1>
+  			
+
+  		</div>
+  		<?php
+  	}
+  	?>
+  	<?php 
+
+
+  	if($flag == 1){
+  		?>
+  		<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  			<h1>User Already Exists</h1>
+  			<button href ="singup.html">Go Back to Sign Up</button>
+  		</div>
+  		<?php
+  	}
+  	?>
+	
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
