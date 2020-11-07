771b553dbb521e55c17b04a8ea80d041631e359f
Huzaifa Saboowala <huzaifa.saboowala96@gmail.com>
2020-11-07 01:12:29 -0500

    Corrected password input type field for additional security. Able to send data from form to database

--- Project/insert.php
+++ Project/insert.php
@@ -4,19 +4,13 @@ $username = "root";
 $password = "";
 $dbname = "hstworld";
 
-//if (isset($_POST)) {
-/*$firstname = $_POST['fname'];
-$lastname = $_POST['lname'];
-$usernmae = $_POST['uname'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$password = $_POST['pswd'];*/
-$firstname = "fname";
-$lastname = "lname";
-$uusername = "uname";
-$email = "email";
-$phone = 4372137721;
-$upassword = "pswd";
+if (isset($_POST)) {
+$firstname = $_POST['FirstName'];
+$lastname = $_POST['LastName'];
+$uusername = $_POST['UserName'];
+$email = $_POST['Email'];
+$phone = $_POST['PhoneNumber'];
+$upassword = $_POST['Password'];
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
  
@@ -28,13 +22,12 @@ if(!$conn){
 $sql = "INSERT INTO `users` (`first_name`, `last_name`, `username`, `email`, `password`, `phone`) 
 VALUES ('$firstname', '$lastname', '$uusername', '$email','$upassword','$phone')";
 /*$sql = "INSERT INTO `signin`(`FirstName`, `LastName`, `UserName`, `Email`, `PhoneNumber`, `Password`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6])";*/
-//}
+}
 if(mysqli_query($conn, $sql)){
     echo "Records added successfully.";
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-}
- 
+} 
 // Close connection
 mysqli_close($conn);
 ?>
